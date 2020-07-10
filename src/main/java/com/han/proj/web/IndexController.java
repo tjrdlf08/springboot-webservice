a395f5458139e34f12e721a9f4952a9e66b2a514
tjrdlf08 <tjrdlf08@gmail.com>
2020-07-10 13:28:42 +0900

    make function posts modify,delete

--- src/main/java/com/han/proj/web/IndexController.java
+++ src/main/java/com/han/proj/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.han.proj.web;
 
 import com.han.proj.service.posts.PostsService;
+import com.han.proj.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,5 +22,13 @@ public class IndexController {                          // 머스테치 스타�
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
